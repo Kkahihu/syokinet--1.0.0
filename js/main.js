5723faab20fb1141eca04dbc5b1976bc9d6474c6
Kelvin Kahihu <kelvinkahihu180226@daystar.ac.ke>
2022-06-24 13:30:52 +0300

    Video controls and responsiveness updated

--- js/main.js
+++ js/main.js
@@ -72,13 +72,69 @@
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsive: {
-            0:{
+            480 :{
                 items:1
             },
             768:{
                 items:2
             }
         }
+    });
+
+    //Youtube Video carousel
+    // $('.video-carousel').owlCarousel({
+    //     autoplay: true,
+    //     loop:true,
+    //     margin:10,
+    //     responsiveClass:true,
+    //     dots: false,
+    //     autoHeight: true,
+    //     responsive:{
+    //         0:{
+    //             responsiveClass: true,
+    //             dots: false,
+    //             nav: false,
+    //             autoHeight: true,
+    //             items: 1
+    //         },
+    //         768 : {
+    //             items: 1
+    //         }
+    //
+    //     }
+    // })
+
+    $(function() {
+        var owl = $('.video-carousel'),
+
+            owlOptions = {
+                autoplay: true,
+                loop: true,
+                margin: 30,
+                smartSpeed: 1500,
+                nav: false,
+                items: 1
+            };
+
+        if ( $(window).width() < 460 ) {
+            var owlActive = owl.owlCarousel(owlOptions);
+        } else {
+            owl.addClass('off');
+        }
+
+        $(window).resize(function() {
+            if ( $(window).width() < 768 ) {
+                if ( $('.owl-carousel').hasClass('off') ) {
+                    var owlActive = owl.owlCarousel(owlOptions);
+                    owl.removeClass('off');
+                }
+            } else {
+                if ( !$('.owl-carousel').hasClass('off') ) {
+                    owl.addClass('off').trigger('destroy.owl.carousel');
+                    owl.find('.owl-stage-outer').children(':eq(0)').unwrap();
+                }
+            }
+        });
     });
 
 
